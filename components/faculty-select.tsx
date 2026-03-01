e23dfd64b940ee3cab04be74cd5b36e46c38b5cd
dheereshag <da11@iitbbs.ac.in>
2026-03-01 20:27:55 +0530

    feat: implement Combobox component and replace Select with Combobox in FacultySelect refactor: update layout title to "Adjustment Finder" and add new UI components chore: add dependencies for cmdk and radix-ui controllable state

--- components/faculty-select.tsx
+++ components/faculty-select.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Combobox,
+  ComboboxContent,
+  ComboboxEmpty,
+  ComboboxGroup,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+  ComboboxTrigger,
+} from "@/components/kibo-ui/combobox";
 
 interface Faculty {
   id: number;
@@ -18,23 +21,32 @@ interface FacultySelectProps {
 }
 
 export default function FacultySelect({ faculties }: FacultySelectProps) {
+  const facultyOptions = faculties.map((faculty) => ({
+    value: String(faculty.id),
+    label: `${faculty.name} (E${faculty.id})`,
+  }));
+
   return (
     <div className="flex flex-col gap-2">
       <label className="text-sm font-medium text-zinc-700 dark:text-zinc-300">
         Requesting Faculty
       </label>
-      <Select>
-        <SelectTrigger className="w-72">
-          <SelectValue placeholder="Select a faculty member" />
-        </SelectTrigger>
-        <SelectContent>
-          {faculties.map((faculty) => (
-            <SelectItem key={faculty.id} value={String(faculty.id)}>
-              {faculty.name}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+      <Combobox data={facultyOptions} type="faculty">
+        <ComboboxTrigger className="w-72" />
+        <ComboboxContent>
+          <ComboboxInput />
+          <ComboboxEmpty />
+          <ComboboxList>
+            <ComboboxGroup>
+              {facultyOptions.map((faculty) => (
+                <ComboboxItem key={faculty.value} value={faculty.value}>
+                  {faculty.label}
+                </ComboboxItem>
+              ))}
+            </ComboboxGroup>
+          </ComboboxList>
+        </ComboboxContent>
+      </Combobox>
     </div>
   );
 }
